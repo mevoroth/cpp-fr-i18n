ef78535a24cc03e7a36f8e8db54bfcfab540aa1c
Daniel Mevoroth Quach <daniel@quach.info>
2016-04-06 23:14:18 +0200

    API

---
+++ Api.h
@@ -0,0 +1,39 @@
+#ifndef _CPP_FR_I18N_API_
+#define _CPP_FR_I18N_API_
+
+#define non_signe		unsigned
+#define signe			signed
+#define booleen			bool
+#define entier			int
+#define caractere		char
+#define flottant		float
+#define vide			void
+#define vrai			true
+#define faux			false
+#define utiliser		using
+#define espace_de_nom	namespace
+#define structure		struct
+#define classe			class
+#define enumeration		enum
+#define union			union
+#define patron			template
+#define virtuel			virtual
+#define retour			return
+#define si				if
+#define sinon			else
+#define tant_que		while
+#define pour			for
+#define faire			do
+#define public			public
+#define prive			private
+#define protege			protected
+#define cas				case
+#define aller_a			goto
+#define casser			break
+#define continuer		continue
+#define nouveau			new
+#define supprimer		delete
+#define taille_de		sizeof
+#define definir_type	typedef
+
+#endif
